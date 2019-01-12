e12b1fb2dd1406102eb2a4d6fc55936acd5d9a4b
Bapsk <baptiste.kief@gmail.com>
2019-01-12 17:31:26 -0500

    last_update

--- app.js
+++ app.js
@@ -6,25 +6,57 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/test.html');
 });
 
+app.get('/puissance4', function(req, res){
+  res.sendFile(__dirname + '/puissance4.html');
+});
+
+
+var partie = {
+	tab : new Array(),
+	initTab : function () {
+		for (var i = 0; i < 7; i++) {
+			this.tab[i] = new Array()
+			for (var j = 0; j < 6; j++) {
+				this.tab[i][j] = 0;
+			}
+		}
+	},
+	joueur1 : '',
+	joueur2 : '' 
+}
+
+partie.initTab();
+
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+	socket.broadcast.emit('tab', partie.tab);
+	socket.emit('tab', partie.tab);
+	console.log(partie.tab);
+	socket.on('disconnect', function(){
+	console.log('user disconnected');
+	});
 
-  socket.on('pseudo', function(pseudo){
-  	socket.pseudo = pseudo;
-  });
+	socket.on('joueur', function(pseudo, couleur){
+		socket.pseudo = pseudo;
+		socket.couleur = couleur;
+	});
 
-  socket.on('room', function(room){
-  	socket.leave(socket.room);
-  	socket.room = room;  	
-  	socket.join(room);
-  });
+	socket.on('tab', function(tableau){
+		partie.tab = tableau;
+		socket.broadcast.emit('tab', partie.tab);
+		socket.emit('tab', partie.tab);
+		console.log(partie.tab);
+	});
 
-  socket.on('message', function(msg){
+
+	socket.on('room', function(room){
+		socket.leave(socket.room);
+		socket.room = room;
+		socket.join(room);
+	});
+
+	socket.on('message', function(msg){
 	io.sockets.in(socket.room).emit('message', {pseudo: socket.pseudo, msg: msg});
-   });
+	});
 
 
 
